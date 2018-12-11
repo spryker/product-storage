42ec70da0043de8e950f234e4611a0f97e63bd37
Aleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2018-12-11 13:00:42 +0000

    tech-135: listener srp violation

--- src/Spryker/Zed/ProductStorage/Communication/Plugin/Event/Listener/ProductAbstractStorageUnpublishListener.php
+++ src/Spryker/Zed/ProductStorage/Communication/Plugin/Event/Listener/ProductAbstractStorageUnpublishListener.php
@@ -13,18 +13,18 @@ use Spryker\Zed\Product\Dependency\ProductEvents;
 use Spryker\Zed\PropelOrm\Business\Transaction\DatabaseTransactionHandlerTrait;
 
 /**
- * @deprecated Use `\Spryker\Zed\ProductStorage\Communication\Plugin\Event\Listener\ProductConcreteStoragePublishListener` and `\Spryker\Zed\ProductStorage\Communication\Plugin\Event\Listener\ProductConcreteStorageUnpublishListener` instead.
- *
  * @method \Spryker\Zed\ProductStorage\Persistence\ProductStorageQueryContainerInterface getQueryContainer()
  * @method \Spryker\Zed\ProductStorage\Communication\ProductStorageCommunicationFactory getFactory()
  * @method \Spryker\Zed\ProductStorage\Business\ProductStorageFacadeInterface getFacade()
  * @method \Spryker\Zed\ProductStorage\ProductStorageConfig getConfig()
  */
-class ProductConcreteStorageListener extends AbstractPlugin implements EventBulkHandlerInterface
+class ProductAbstractStorageUnpublishListener extends AbstractPlugin implements EventBulkHandlerInterface
 {
     use DatabaseTransactionHandlerTrait;
 
     /**
+     * {@inheritdoc}
+     *
      * @api
      *
      * @param \Generated\Shared\Transfer\EventEntityTransfer[] $eventTransfers
@@ -35,14 +35,12 @@ class ProductConcreteStorageListener extends AbstractPlugin implements EventBulk
     public function handleBulk(array $eventTransfers, $eventName)
     {
         $this->preventTransaction();
-        $productIds = $this->getFactory()->getEventBehaviorFacade()->getEventTransferIds($eventTransfers);
+        $productAbstractIds = $this->getFactory()->getEventBehaviorFacade()->getEventTransferIds($eventTransfers);
 
-        if ($eventName === ProductEvents::ENTITY_SPY_PRODUCT_DELETE ||
-            $eventName === ProductEvents::PRODUCT_CONCRETE_UNPUBLISH
+        if ($eventName === ProductEvents::ENTITY_SPY_PRODUCT_ABSTRACT_DELETE ||
+            $eventName === ProductEvents::PRODUCT_ABSTRACT_UNPUBLISH
         ) {
-            $this->getFacade()->unpublishConcreteProducts($productIds);
-        } else {
-            $this->getFacade()->publishConcreteProducts($productIds);
+            $this->getFacade()->unpublishProductAbstracts($productAbstractIds);
         }
     }
 }
